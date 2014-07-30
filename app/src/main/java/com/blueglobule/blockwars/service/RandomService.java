31131ace6f674ae085d3fa272e0fea39dea90b41
Pierre BINAULD <pierre.binauld+github@gmail.com>
2014-07-30 16:59:12 +0200

    Implementation of random and time services

--- app/src/main/java/com/blueglobule/blockwars/service/RandomService.java
+++ app/src/main/java/com/blueglobule/blockwars/service/RandomService.java
@@ -1,4 +1,12 @@
 package com.blueglobule.blockwars.service;
 
+import java.util.Random;
+
 public class RandomService {
+
+    Random random = new Random();
+
+    public int random(int min, int max) {
+        return random.nextInt((max - min) + 1) + min;
+    }
 }
--- app/src/main/java/com/blueglobule/blockwars/service/TimeService.java
+++ app/src/main/java/com/blueglobule/blockwars/service/TimeService.java
@@ -1,4 +1,21 @@
 package com.blueglobule.blockwars.service;
 
 public class TimeService {
+    long startTime;
+
+    public void start() {
+        startTime = System.nanoTime();
+    }
+
+    public long elapsedNanoSeconds() {
+        return System.nanoTime() - startTime;
+    }
+
+    public long elapsedMilliSeconds() {
+        return (long) ((System.nanoTime() - startTime) / 1e6);
+    }
+
+    public long elapsedSeconds() {
+        return (long) ((System.nanoTime() - startTime) / 1e9);
+    }
 }
